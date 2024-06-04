55120d99ecfa01fa2c7aad35496c2d638f710cc7
Charles D. <e38961@promotion-sociale.be>
2024-06-04 17:42:00 +0200

    Structure MVC initiale

---
+++ app/config/params.php
@@ -0,0 +1,10 @@
+<?php 
+// PARAMS
+
+// Paramètres de connexion a la  DB
+
+define('DB_HOST', 'localhost:3306');
+define('DB_NAME', 'book_hunter_2024');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+?>
