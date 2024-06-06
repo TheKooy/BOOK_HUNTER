906659a1011c9bac30df8861b428fdfad7d3212a
Charles D. <e38961@promotion-sociale.be>
2024-06-06 14:48:44 +0200

    Route books.index + authors.index
    Avec les partials _index

--- app/routers/index.php
+++ app/routers/index.php
@@ -1,11 +1,28 @@
 <?php 
 //ROUTER PRINCIPAL 
 
+// ROUTE DES AUTHORS
+// PATTERN: /?authors
+// CTRL: authorsController
+// ACTION: indexAction
+if (isset($_GET['authors'])) :
+    include_once '../app/controllers/authorsController.php';
+    \App\Controllers\AuthorsController\indexAction($connexion);
+
+// ROUTE DES BOOKS
+// PATTERN: /?books
+// CTRL: booksController
+// ACTION: indexAction
+elseif (isset($_GET['books'])) :
+    include_once '../app/controllers/booksController.php';
+    \App\Controllers\BooksController\indexAction($connexion);
 
 // ROUTE PAR DEFAUT
 // PATTERN: /
 // CTRL: pagesController
 // ACTION: homeAction
+else :
 include_once '../app/controllers/pagesController.php';
 \App\Controllers\PagesController\homeAction($connexion);
+endif;
 ?>
